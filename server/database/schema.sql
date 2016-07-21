73efda8510aad89f11d49b3c7747550abd60a5ee
Festus Kiprop <Fyangani@gmail.com>
2016-07-21 04:24:50 -0500

    Update schema.sql

--- server/database/schema.sql
+++ server/database/schema.sql
@@ -1,5 +1,6 @@
 CREATE TABLE articles (
   id     INT PRIMARY KEY AUTO_INCREMENT,
   title  VARCHAR(100) NOT NULL,
-  body   TEXT         NOT NULL
+  body   TEXT         NOT NULL,
+  UNIQUE(title)
 );
